1167da070c7628086c8be8918307658b7848580c
Dmytro <avida.d3@gmail.com>
2026-07-21 17:22:51 +0300

    Implement some shit

--- src/class_loader.rs
+++ src/class_loader.rs
@@ -1,4 +1,8 @@
 pub mod class_loader {
+
+    use byteorder::BigEndian;
+    use byteorder::ReadBytesExt;
+
     use crate::java_class::java_class::*;
     use std::fmt;
     use std::fs;
@@ -29,6 +33,7 @@ pub mod class_loader {
             .and_then(|r| Ok(r))
             .map_err(|_| ClassLoadError::NotFound("File not found".to_string()))
     }
+
     pub fn parse(bytes: Vec<u8>) -> Result<JavaClass, ClassLoadError> {
         let mut cursor = Cursor::new(bytes);
         let mut buf = [0u8; 4];
@@ -50,14 +55,112 @@ pub mod class_loader {
             ClassLoadError::InvalidFormat(format!("Failed reading constant pool count: {}", e))
         })?;
         let constant_pool_count = u16::from_be_bytes([buf[0], buf[1]]);
+        let constant_pool = parse_constant_pool(&mut cursor, constant_pool_count)?;
 
         Ok(JavaClass {
             magic_number,
             minor_version,
             major_version,
             constant_pool_count,
+            constant_pool,
         })
     }
+
+    fn map_error(_: std::io::Error) -> ClassLoadError {
+        ClassLoadError::InvalidFormat("Error parsing constant fields".to_string())
+    }
+    fn parse_constant_pool_info(
+        cursor: &mut Cursor<Vec<u8>>,
+        tag: ConstantPoolTag,
+    ) -> Result<FieldInfo, ClassLoadError> {
+        match tag {
+            ConstantPoolTag::Utf8 => {
+                let length = cursor.read_u16::<byteorder::BigEndian>().unwrap();
+                let mut bytes: Vec<u8> = vec![0u8; length as usize];
+                cursor.read_exact(&mut bytes).map_err(map_error)?;
+                Ok(FieldInfo::Utf8Info { length, bytes })
+            }
+            ConstantPoolTag::Integer => {
+                Err(ClassLoadError::Other("unimplemented: Integer".to_string()))
+            }
+            ConstantPoolTag::Float => {
+                Err(ClassLoadError::Other("unimplemented: Float".to_string()))
+            }
+            ConstantPoolTag::Long => Err(ClassLoadError::Other("unimplemented: Long".to_string())),
+            ConstantPoolTag::Double => {
+                Err(ClassLoadError::Other("unimplemented: Double".to_string()))
+            }
+            ConstantPoolTag::Class => Ok(FieldInfo::ClassInfo {
+                name_index: cursor.read_u16::<byteorder::BigEndian>().unwrap(),
+            }),
+            ConstantPoolTag::String => {
+                Err(ClassLoadError::Other("unimplemented: String".to_string()))
+            }
+            ConstantPoolTag::Fieldref => {
+                Err(ClassLoadError::Other("unimplemented: Fieldref".to_string()))
+            }
+            ConstantPoolTag::Methodref => {
+                // let mut buf = [0u8; std::mem::size_of::<RefFieldInfo>()];
+                // cursor.read_exact(&mut buf).map_err(map_error)?;
+                Ok(FieldInfo::MethodRef(RefFieldInfo {
+                    class_index: cursor.read_u16::<byteorder::BigEndian>().unwrap(),
+                    name_and_type_index: cursor.read_u16::<byteorder::BigEndian>().unwrap(),
+                }))
+            }
+            ConstantPoolTag::InterfaceMethodref => Err(ClassLoadError::Other(
+                "unimplemented: InterfaceMethodref".to_string(),
+            )),
+            ConstantPoolTag::NameAndType => Ok(FieldInfo::NameAndType {
+                name_index: cursor.read_u16::<byteorder::BigEndian>().unwrap(),
+                descriptor_index: cursor.read_u16::<byteorder::BigEndian>().unwrap(),
+            }),
+            ConstantPoolTag::MethodHandle => Err(ClassLoadError::Other(
+                "unimplemented: MethodHandle".to_string(),
+            )),
+            ConstantPoolTag::MethodType => Err(ClassLoadError::Other(
+                "unimplemented: MethodType".to_string(),
+            )),
+            ConstantPoolTag::Dynamic => {
+                Err(ClassLoadError::Other("unimplemented: Dynamic".to_string()))
+            }
+            ConstantPoolTag::InvokeDynamic => Err(ClassLoadError::Other(
+                "unimplemented: InvokeDynamic".to_string(),
+            )),
+            ConstantPoolTag::Module => {
+                Err(ClassLoadError::Other("unimplemented: Module".to_string()))
+            }
+            ConstantPoolTag::Package => {
+                Err(ClassLoadError::Other("unimplemented: Package".to_string()))
+            }
+        }
+    }
+
+    fn parse_constant_pool(
+        cursor: &mut Cursor<Vec<u8>>,
+        count: u16,
+    ) -> Result<Vec<ConstantPoolInfo>, ClassLoadError> {
+        let mut constant_pool: Vec<ConstantPoolInfo> = Vec::new();
+
+        let mut next_tag: u8 = 0;
+        for _ in 0..count - 1 {
+            cursor
+                .read_exact(std::slice::from_mut(&mut next_tag))
+                .map_err(|e| {
+                    ClassLoadError::InvalidFormat(format!(
+                        "Failed reading constant pool info: {}",
+                        e
+                    ))
+                })?;
+            println!("next Tag is {}", next_tag);
+            let tag = ConstantPoolTag::try_from(next_tag).unwrap();
+            println!("Tag is {}", tag);
+            let info = parse_constant_pool_info(cursor, tag)
+                .map_err(|e| ClassLoadError::InvalidFormat("Bad Field info".to_string()))?;
+            constant_pool.push(ConstantPoolInfo { tag, info });
+        }
+        Ok(constant_pool)
+    }
+
     pub fn load(name: &str) -> Result<JavaClass, ClassLoadError> {
         let data = read(name)?;
         parse(data)
--- src/java_class.rs
+++ src/java_class.rs
@@ -1,11 +1,142 @@
 pub mod java_class {
     use std::fmt;
+
+    #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+    #[repr(u8)]
+    pub enum ConstantPoolTag {
+        Utf8 = 1,
+        Integer = 3,
+        Float = 4,
+        Long = 5,
+        Double = 6,
+        Class = 7,
+        String = 8,
+        Fieldref = 9,
+        Methodref = 10,
+        InterfaceMethodref = 11,
+        NameAndType = 12,
+        MethodHandle = 15,
+        MethodType = 16,
+        Dynamic = 17,
+        InvokeDynamic = 18,
+        Module = 19,
+        Package = 20,
+    }
+
+    impl TryFrom<u8> for ConstantPoolTag {
+        type Error = ();
+
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
+            match value {
+                1 => Ok(ConstantPoolTag::Utf8),
+                3 => Ok(ConstantPoolTag::Integer),
+                4 => Ok(ConstantPoolTag::Float),
+                5 => Ok(ConstantPoolTag::Long),
+                6 => Ok(ConstantPoolTag::Double),
+                7 => Ok(ConstantPoolTag::Class),
+                8 => Ok(ConstantPoolTag::String),
+                9 => Ok(ConstantPoolTag::Fieldref),
+                10 => Ok(ConstantPoolTag::Methodref),
+                11 => Ok(ConstantPoolTag::InterfaceMethodref),
+                12 => Ok(ConstantPoolTag::NameAndType),
+                15 => Ok(ConstantPoolTag::MethodHandle),
+                16 => Ok(ConstantPoolTag::MethodType),
+                17 => Ok(ConstantPoolTag::Dynamic),
+                18 => Ok(ConstantPoolTag::InvokeDynamic),
+                19 => Ok(ConstantPoolTag::Module),
+                20 => Ok(ConstantPoolTag::Package),
+                _ => Err(()),
+            }
+        }
+    }
+
+    impl fmt::Display for ConstantPoolTag {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let s = match self {
+                ConstantPoolTag::Utf8 => "Utf8",
+                ConstantPoolTag::Integer => "Integer",
+                ConstantPoolTag::Float => "Float",
+                ConstantPoolTag::Long => "Long",
+                ConstantPoolTag::Double => "Double",
+                ConstantPoolTag::Class => "Class",
+                ConstantPoolTag::String => "String",
+                ConstantPoolTag::Fieldref => "Fieldref",
+                ConstantPoolTag::Methodref => "Methodref",
+                ConstantPoolTag::InterfaceMethodref => "InterfaceMethodref",
+                ConstantPoolTag::NameAndType => "NameAndType",
+                ConstantPoolTag::MethodHandle => "MethodHandle",
+                ConstantPoolTag::MethodType => "MethodType",
+                ConstantPoolTag::Dynamic => "Dynamic",
+                ConstantPoolTag::InvokeDynamic => "InvokeDynamic",
+                ConstantPoolTag::Module => "Module",
+                ConstantPoolTag::Package => "Package",
+            };
+            write!(f, "{}", s)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct RefFieldInfo {
+        pub class_index: u16,
+        pub name_and_type_index: u16,
+    }
+    #[derive(Debug)]
+    pub enum FieldInfo {
+        ClassInfo { name_index: u16 },
+        Utf8Info {
+            length: u16,
+            bytes: Vec<u8>,
+
+        },
+        NameAndType{
+            name_index: u16,
+            descriptor_index: u16
+        },
+
+        MethodRef(RefFieldInfo),
+        InterfaceMethodRef(RefFieldInfo),
+        FieldRef(RefFieldInfo),
+    }
+
+    impl fmt::Display for RefFieldInfo {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "class_index: {}, name_and_type_index: {}",
+                self.class_index, self.name_and_type_index
+            )
+        }
+    }
+
+    impl fmt::Display for FieldInfo {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                FieldInfo::ClassInfo { name_index } => {
+                    write!(f, "ClassInfo name_index={}", name_index)
+                }
+                FieldInfo::Utf8Info { length, bytes } => {
+                    let text = String::from_utf8_lossy(bytes);
+                    write!(f, "Utf8Info length={} text={}", length, text)
+                }
+                FieldInfo::MethodRef(r) => write!(f, "MethodRef {}", r),
+                FieldInfo::InterfaceMethodRef(r) => write!(f, "InterfaceMethodRef {}", r),
+                FieldInfo::FieldRef(r) => write!(f, "FieldRef {}", r),
+                _ => write!(f, "Unimplemented")
+            }
+        }
+    }
+    #[derive(Debug)]
+    pub struct ConstantPoolInfo {
+        pub tag: ConstantPoolTag,
+        pub info: FieldInfo,
+    }
     #[derive(Debug)]
     pub struct JavaClass {
         pub magic_number: u32,
         pub minor_version: u16,
         pub major_version: u16,
         pub constant_pool_count: u16,
+        pub constant_pool: Vec<ConstantPoolInfo>,
     }
     impl Default for JavaClass {
         fn default() -> Self {
@@ -14,6 +145,7 @@ pub mod java_class {
                 minor_version: 0,
                 major_version: 0,
                 constant_pool_count: 0,
+                constant_pool: vec![],
             }
         }
     }
